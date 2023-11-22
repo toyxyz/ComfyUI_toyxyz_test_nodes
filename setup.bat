f1fe8230399c3eefecf42761ad2b8dc39817b2f1
toyxyz <toyxyz@naver.com>
2023-11-23 02:54:43 +0900

    Add files via upload

---
+++ setup.bat
@@ -0,0 +1,13 @@
+@echo off
+
+IF NOT EXIST venv (
+    echo Creating venv...
+    python -m venv venv
+)
+
+:: Deactivate the virtual environment
+call .\venv\Scripts\deactivate.bat
+
+call .\venv\Scripts\activate.bat
+
+pip install -r requirements.txt

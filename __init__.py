052a0d433e4ec98aad198d3eb94a275251f9e329
toyxyz <toyxyz@naver.com>
2025-03-10 03:57:31 +0900

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -2,7 +2,7 @@ import importlib
 import os
 
 node_list = [ #Add list of .py files containing nodes here
-    "toyxyz_test_nodes"
+    "toyxyz_test_nodes", "visual_area_mask"
 ]
 
 NODE_CLASS_MAPPINGS = {}
@@ -15,3 +15,5 @@ for module_name in node_list:
     NODE_DISPLAY_NAME_MAPPINGS = {**NODE_DISPLAY_NAME_MAPPINGS, **imported_module.NODE_DISPLAY_NAME_MAPPINGS}
 
 __all__ = ['NODE_CLASS_MAPPINGS', 'NODE_DISPLAY_NAME_MAPPINGS']
+
+WEB_DIRECTORY = "./web"
